a5d50e34d46b04aa7c82a54f8dbca1f834624606
BuildTools <unconfigured@null.spigotmc.org>
2017-02-24 17:32:57 +0000

    Added documentation, created factory_1996 method

--- src/main.rs
+++ src/main.rs
@@ -4,26 +4,54 @@ use rand::Rng;
 use rand::distributions::{IndependentSample, Range};
 
 fn factory_1994(rand: u32) -> String{
-	let s : String = format!("{}", rand);
-	s
-	
-	
+	let mut s = String::new();
+	match rand {
+		1...31 => s = "Brown".to_string(),
+		31...51 => s = "Yellow".to_string(),
+		51...71 => s = "Red".to_string(),
+		71...81 => s = "Orange".to_string(),
+		81...91 => s = "Tan".to_string(),
+		91...101 => s = "Green".to_string(),
+		_ => s = "Invalid".to_string(),
+	}
+	s	
+}
+
+fn factory_1996(rand: u32) -> String{
+        let mut s = String::new();
+        match rand {
+                1...25 => s = "Blue".to_string(),
+                25...45 => s = "Green".to_string(),
+                45...61 => s = "Orange".to_string(),
+                61...75 => s = "Yellow".to_string(),
+                75...88 => s = "Red".to_string(),
+                88...101 => s = "Brown".to_string(),
+                _ => s = "Invalid".to_string(),
+        }
+        s
 }
 
 fn main() {
-	let mut rng = rand::thread_rng();
-	let mut range = Range::new(1, 101);
-	let read = io::stdin();
-	let mut input = String::new();
+	let mut rng = rand::thread_rng(); //New Random number gen
+	let mut range = Range::new(1, 101); //Define range for random number
+	let read = io::stdin(); 
+	let mut input = String::new(); //line input string
+
 	println!("Enter number of itterations: ");
 	read.read_line(&mut input);
-	let mut random;
-	let mut test;
+
+	let mut random; //random number
+	let mut old; //old M and M
+	let mut new; //new M and m
+	//parse number of itterations from stdin
 	let mut itt = input.trim().parse::<i32>().expect("invalid input");
 	while(itt != 0){
 		random = range.ind_sample(&mut rng);
-		test = factory_1994(random);
-		println!("{}", test);
+		old = factory_1994(random);
+		random = range.ind_sample(&mut rng);
+		new = factory_1996(random);
+		println!("Old choice: {}", old);
+		println!("New Choice: {}", new);
 		itt -= 1;
 	}
 
